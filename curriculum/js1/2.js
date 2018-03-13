e31cb65958b2050d88adef792dc82400a24e8199
Huy Vo <huy@garagescript.org>
2018-03-12 20:17:41 -0700

    Fix 2

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -8,15 +8,15 @@
 
 const solution = (a, b, c) => {
 
-  if (a >b && a>c)
-  {
+  if (a >b && a>c){
   return a;
   }
-  if (b>a && b>c)
-  {
+
+  if (b>a && b>c){
   return b;
   }
-else return c;
+
+  else return c;
   
 };
 
